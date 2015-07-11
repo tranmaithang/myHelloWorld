fb0dd7c54d1589b13570aa8b074327b152d89c2e
tranmaithang <tranmaithang@gmail.com>
2015-07-11 14:10:30 +0200

    [QA] Add project 'asymmetry_word'

---
+++ myQA/algorithm/asymmetry_word/asymmetry_word.cpp
@@ -0,0 +1,47 @@
+// ALGORITHM 
+// Check asymmetry of a word 
+// Interview : Societe Generale - PnL Analyst - 06-Nov-2014
+
+// Author : TMT
+// Created on November 16, 2014, 12:27 PM 
+
+#include <iostream>
+#include <string>
+#include <array>
+using namespace std;
+
+bool isAsymmetry(string aString)
+{
+	bool test = true;
+	for (size_t i = 0; i < aString.size(); i++)
+	{
+		char vChar = aString[i];
+		char vChar1 = aString[aString.size() - i];
+		if (aString[i] != aString[aString.size() - i - 1])
+		{
+			test = false;
+			break;
+		}
+	}
+	return test;
+}
+
+int main()
+{
+	string str;
+	string myString;
+
+	cout << "=== CHECK ASYMMETRY OF A WORD ===" << endl;
+	do
+	{
+		cout << endl;
+		cout << "Give a word : ";
+		cin >> myString;
+		cout << myString << " is asymmetric ? : " << isAsymmetry(myString) << endl;
+		cout << endl;
+		cout << "Do it again (y/n) ?";
+		cin >> str;
+	} 
+	while (str != "n");
+	return 0;
+}
